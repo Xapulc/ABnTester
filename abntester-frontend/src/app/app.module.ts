5c85aac63c1b3124d4d6313e853b2361780b18ad
Victor Kharlamov <vi.v.kharlamov@gmail.com>
2023-08-18 13:55:31 +0300

    Replace Yandex Metric to Angular module

--- abntester-frontend/src/app/app.module.ts
+++ abntester-frontend/src/app/app.module.ts
@@ -44,6 +44,7 @@ import {StandardCalculationResultComponent} from './standard-calculation-result/
 import {HIGHLIGHT_OPTIONS, HighlightModule} from 'ngx-highlightjs';
 import {MdeComponent} from './parameters/mde/mde.component';
 import {ClipboardModule} from '@angular/cdk/clipboard';
+import {MetrikaModule} from "ng-yandex-metrika";
 
 @NgModule({
   declarations: [
@@ -86,7 +87,15 @@ import {ClipboardModule} from '@angular/cdk/clipboard';
     HighlightModule,
     ClipboardModule,
     TuiLinkModule,
-    TuiDropdownModule
+    TuiDropdownModule,
+    MetrikaModule.forRoot(
+      {id: 94541330,
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+      }, // CounterConfig | CounterConfig[]
+    ),
   ],
   providers: [{provide: LocationStrategy, useClass: HashLocationStrategy},
     {provide: HTTP_INTERCEPTORS, useClass: GlobalHttpInterceptor, multi: true},
